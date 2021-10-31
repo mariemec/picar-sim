7f9a21f6929652969dcf5c68afc0d752b3e4acaa
Marie-Eve C <casm1907@usherbrooke.ca>
2021-10-31 10:34:05 -0400

    add position class and bille class

---
+++ logic/bille.py
@@ -0,0 +1,16 @@
+import numpy as np
+from position import Position
+
+
+class Bille:
+
+    def __init__(self, theta=0, x=0, y=0):
+        self.mass = 0.005
+        self.radius_bille = 0.016
+        self.radius_pendule = 0.14
+        self.position = Position(x, y)
+        self.theta = theta
+
+    def calculate_next_pos(self):
+        # TO-DO
+        return 0
