f5a4aeb4c38ec7d5a620ed64ce48c23cd6795043
Marie-Eve Castonguay <>
2021-12-01 20:57:33 -0500

    add draw_track

---
+++ logic/draw_track.py
@@ -0,0 +1,16 @@
+import numpy as np
+from PIL import Image
+
+## After calling the following method on sim.trajectoire.map
+# def print_map_to_file(self):
+#     rotated = [[self._map[j][i] for j in range(len(self._map))] for i in range(len(self._map[0]) - 1, -1, -1)]
+#     my_track = open("output.txt", "w")
+#     for row in rotated:
+#         np.savetxt(my_track, row)
+#     my_track.close()
+
+# Use this main to generate a black and white image of the track
+if __name__ == '__main__':
+    original_array = np.loadtxt("output.txt").reshape(250, 250).astype('uint8')*255
+    im = Image.fromarray(original_array)
+    im.save('track.png')
